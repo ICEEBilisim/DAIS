6a637f83093e5fe2d858df3ff78a8fcd1826e43c
calik <calik@example.com>
2026-05-09 09:01:45 +0300

    fix(web): update whatsapp link with correct ID

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -104,7 +104,7 @@ function App() {
                   <span className="text-orange-500 font-medium ml-1">| dais@iceebilisim.com</span>
                 </div>
                 <span className="mx-2 text-slate-300 hidden sm:inline">|</span>
-                <a href="https://chat.whatsapp.com/C63NIIH1vimLtGCM3pqzbm" target="_blank" rel="noopener noreferrer" className="text-emerald-500 hover:text-emerald-600 font-medium transition-colors flex items-center gap-1 w-full sm:w-auto mt-1 sm:mt-0">
+                <a href="https://chat.whatsapp.com/C63NIlH1vimLtGCM3pqzbm" target="_blank" rel="noopener noreferrer" className="text-emerald-500 hover:text-emerald-600 font-medium transition-colors flex items-center gap-1 w-full sm:w-auto mt-1 sm:mt-0">
                   <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
                   WhatsApp Grubu
                 </a>
